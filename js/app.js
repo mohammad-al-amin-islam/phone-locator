f3953f31b09545a70b248e6e797fd564af910bd6
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-02-28 22:05:20 +0600

    fetch the data and console it

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log('connected');
+document.getElementById('button').addEventListener('click', function () {
+    const getInputText = document.getElementById('input-text');
+    const getInputTextValue = getInputText.value.toLowerCase();
+
+    const url = `https://openapi.programming-hero.com/api/phones?search=${getInputTextValue}`
+    console.log(url);
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>console.log(data));
+})

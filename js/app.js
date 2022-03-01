b404751d98e9769892822eb1dbb804d0f852f97d
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-03-01 11:01:52 +0600

    details phone info area added

--- js/app.js
+++ js/app.js
@@ -13,12 +13,11 @@ document.getElementById('button').addEventListener('click', function () {
 const displayPhone = phones => {
     const displayPhoneHolder = document.getElementById('phone-display');
     displayPhoneHolder.textContent = "";
-    if(phones.length == 0){
+    if (phones.length == 0) {
         document.getElementById('notfound').style.display = "block";
     }
     phones.forEach(phone => {
         const div = document.createElement('div');
-        console.log(phone);
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100 shadow-sm rounded p-5 border-0">
@@ -26,7 +25,7 @@ const displayPhone = phones => {
             <div class="card-body">
             <h5 class="card-title">${phone.phone_name}</h5>
             <p>Brand: ${phone.brand}</p>
-            <a href="#" onclick="phoneDetails('${phone.slug}')" class="btn btn-primary mt-1">Details</a>
+            <a href="#details-holder" onclick="phoneDetails('${phone.slug}')" class="btn btn-primary mt-1">Details</a>
             </div>
         </div>
 
@@ -36,9 +35,30 @@ const displayPhone = phones => {
 }
 
 //phone details ata fetch area
-const phoneDetails =(name) =>{
+const phoneDetails = (name) => {
     const url = `https://openapi.programming-hero.com/api/phone/${name}`
     fetch(url)
-    .then(res =>res.json())
-    .then( data =>console.log(data.data));
+        .then(res => res.json())
+        .then(data => displayPhoneDetails(data.data));
+}
+
+const displayPhoneDetails = phone => {
+    console.log(phone);
+    const detailsHolder = document.getElementById('details-holder');
+    detailsHolder.textContent = "";
+    const div = document.createElement('div');
+    div.innerHTML = `
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${phone.name}</h5>
+              
+              <p class="card-text"><span class="fw-bold">Release Date: </span>${phone.releaseDate ? phone.releaseDate : 'No release date found'}</p>
+              <p class="card-text"><span class="fw-bold">Chipset: </span>${phone.mainFeatures.chipSet}</p>
+              <p class="card-text"><span class="fw-bold">Display Size: </span>${phone.mainFeatures.displaySize}</p>
+              <p class=""><span class="fw-bold">Memory: </span>${phone.mainFeatures.memory}</p>
+              <p class="card-text"><span class="fw-bold">Storage: </span>${phone.mainFeatures.storage}</p>
+            </div>
+    
+    `
+    detailsHolder.appendChild(div);
 }
